623ef08d98eb841b748d39b7a7322ee9e90b8354
almuzahidseyam <almuzahidseyam@gmail.com>
2026-09-24 07:03:52 +0600

    feat: use modern vercel serverless api pattern for django

---
+++ api/index.py
@@ -0,0 +1,9 @@
+﻿import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cptracker_project.settings")
+app = get_wsgi_application()
