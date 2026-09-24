386e84c79ce7428ad184a93f7be7f0a21ac0bdf5
almuzahidseyam <almuzahidseyam@gmail.com>
2026-09-24 07:05:40 +0600

    fix: remove UTF-8 BOM from vercel.json that broke vercel parser

--- api/index.py
+++
@@ -1,9 +0,0 @@
-﻿import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cptracker_project.settings")
-app = get_wsgi_application()
